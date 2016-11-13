69081c82712059285b6ddff2be0bfd5b1446fc80
alexkirill <sapr2000@mail.ru>
2016-11-13 18:55:14 +0200

    config add and more

--- nodmcu.ino
+++ nodmcu.ino
@@ -6,7 +6,7 @@
 #include <Servo.h>
 #include <SimpleTimer.h>
 #include <ESP8266HTTPClient.h>
-
+#include <MD5Builder.h>
 extern "C" {
   #include "user_interface.h"
 }
@@ -19,16 +19,19 @@ Servo servo;
  * Flash button is convenient to use but if it is pressed it will stuff up the serial port device driver 
  * until the computer is rebooted on windows machines.
  */
-String DEV_VERSION = "1.0";
-const int POWERCHECK_INTERVAL = 10000; // Check battery volage
-float VOLTAGE_THRESHOLD = 2710; // voltage threshold , mv
-const int REPOSITORY_INTERVAL = 5000; //Repository interval check, milisecond
-const char* HOST = "http://345.kiev.ua/1.php"; //Repository  full adress for remote job
-unsigned int UPDPORT = 28031;
-
 const int TRIGGER_PIN = D3; // D3 on NodeMCU and WeMos.
 const int LED_PIN = D7; //Diod pin
 const int MOTOR_PIN = D5; //Motor pin
+const String DEV_VERSION = "1.0";
+const int POWERCHECK_INTERVAL = 600000; // 10 min Check battery volage
+const float VOLTAGE_THRESHOLD = 2710; // voltage threshold , mv
+const int BROADCAST_INTERVAL = 5000; // 5 sec broadcastinterval, milisecond
+unsigned int UPDPORT = 28031;
+
+int REPOSITORY_INTERVAL = 120000; // 2 min Repository interval check, milisecond
+char* REPOSITORY_HOST = "http://345.kiev.ua/1.php"; //Repository  full adress for remote job
+String DID = ""; //device ID
+String DHEX = ""; //Device md5 hex id based
 
 /*
  * Alternative trigger pin. Needs to be connected to a button to use this pin. It must be a momentary connection
@@ -39,11 +42,14 @@ const int MOTOR_PIN = D5; //Motor pin
 bool initialConfig = false; // Indicates whether ESP has WiFi credentials saved from previous session
 ADC_MODE(ADC_VCC); //for voltage measuring
 std::unique_ptr<ESP8266WebServer> server;
+MD5Builder builder_md5;
 SimpleTimer timer_remote;
 SimpleTimer timer_vcc;
+SimpleTimer timer_broadcast;
 WiFiUDP udp;
 
 //-------------main functions----------------------------------------
+/* delete in prodaction
   void goFeed(String sval) {
         int ival = sval.toInt();
         servo.write(ival);
@@ -52,7 +58,8 @@ WiFiUDP udp;
           strcat(msg, sval.c_str());
           server->send(200, "text/plain", msg);
   }
-  void goFeedSlow(String sval) {
+*/
+  void doFeed(String sval) {
         int ival = sval.toInt();
         for(int a=0; a<ival; a++){
               for(int i=0; i<180; i++){
@@ -66,13 +73,15 @@ WiFiUDP udp;
                  delay(5);
                  }
                  delay(200); // for fill the bank))
-                 //servo.write(0); delay(500);  // replaced by for for noise reduce
         }
+        server->send(200, "text/plain", "OK");
+        /* delete in prodaction
           char msg[100];
           strcpy(msg, "Performed cycles : ");
           strcat(msg, sval.c_str());
-          server->send(200, "text/plain", msg);
+          
           Serial.println("Eat and be quiet.");
+          */
   }
   void goblink(int i){ 
     if ((i%10) == 0){ 
@@ -89,30 +98,29 @@ WiFiUDP udp;
   }
   void checkRepository(){
           HTTPClient http;
-          http.begin(HOST);
+          http.begin(REPOSITORY_HOST);
           http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-          http.POST("id=" + getID(WiFi.macAddress()) + "&version=" + DEV_VERSION);
+          http.POST("id=" + getID() + "&version=" + DEV_VERSION);
           String response = http.getString();
           http.end();
           CheckRemoteFeedback(response);  
-          // UPD broadcast also
-          broadcast();
  }
  void CheckRemoteFeedback(String json){
      StaticJsonBuffer<200> jsonBuffer;
      JsonObject& jobj = jsonBuffer.parseObject(json);
      if (!jobj.success()){
-         Serial.println("parseObject() failed");
+         Serial.println("parseObject() failed on CheckRemoteFeedback");
          return;
          }
          //check server answer here. We are here only if json has been parsed successfuly
-         String remote_job = jobj["remote_job"];
          String devise_id = jobj["devise_id"];
-         if(remote_job[0] != '\0'){
-          goFeedSlow(remote_job);
+         String portion = jobj["portion"];
+         if(devise_id[0] != '\0'){
+          doFeed(portion);
          } 
  }
- String getID(String str){
+ String getID(){
+  String str = WiFi.macAddress();
   int j=0;
   String dev_id;
   for(int i=0; i <17; i++){
@@ -120,50 +128,103 @@ WiFiUDP udp;
       }
       return dev_id;
 }
+String getHex(String did) {
+  String doubledid = did + did;
+  builder_md5.begin();
+  builder_md5.add(String(doubledid));
+  builder_md5.calculate();
+  return builder_md5.toString();
+}
 void batteryCheck(){
   float vdd = ESP.getVcc();
   if( vdd < VOLTAGE_THRESHOLD ){
    Serial.print("Curent voltage: "); Serial.println(String(vdd));   
   }
 }
-void setConfigParams(String repository_host, String repository_int_check){
-  Serial.println(String(repository_host));
-  Serial.println(String(repository_int_check));
+void checkConfig(){
+         SPIFFS.begin();
+       if (!SPIFFS.open("/formated.txt", "r")){
+             SPIFFS.format();
+             File format = SPIFFS.open("/formated.txt", "w+");
+             if (format) { 
+                     format.close();
+                     Serial.println("SPIFFS has been formated");
+                     File configFile = SPIFFS.open("/config.txt", "w+");
+                     if (configFile)  {Serial.println("config has been created"); }
+                         }
+            }
+}
+void setConfigParams(String repository_host, String repository_interval){
+      File configFile = SPIFFS.open("/config.txt", "w+");
+       if (configFile) {
+       StaticJsonBuffer<200> jsonBuffer;
+       JsonObject& data = jsonBuffer.createObject();
+          data["repository_host"] = repository_host;
+          data["repository_interval"] = repository_interval;
+       data.printTo(configFile);
+       configFile.close();
+       }
+}
+String getConfigParam(String name){
+       StaticJsonBuffer<200> jsonBuffer; 
+       SPIFFS.begin();
+       File config = SPIFFS.open("/config.txt", "r");
+     if (config) {
+          String line = config.readStringUntil('\n');
+          config.close();
+          JsonObject& jdata = jsonBuffer.parseObject(line);
+          String paramValue = jdata[name];
+          return paramValue; 
+          
+     }else{
+          return "";
+     }
+}
+void initVars(){
+ DID = getID(); 
+ DHEX = getHex(DID);
+                            
+  String rhost = getConfigParam("repository_host");
+  if(rhost != ""){ 
+    char *cstr = new char[rhost.length() + 1];
+    rhost.toCharArray(cstr, rhost.length() + 1); 
+ REPOSITORY_HOST = cstr;
+  }
+  String rint = getConfigParam("repository_interval");
+  if(rint != ""){ 
+ REPOSITORY_INTERVAL = rint.toInt()*60*1000;
+  } 
+}
+void setStaticIP(String ip, String gw, String sn){
+       File configFile = SPIFFS.open("/wificonfig.txt", "w+");
+       if (configFile) {
+       StaticJsonBuffer<200> jsonBuffer;
+       JsonObject& data = jsonBuffer.createObject();
+          data["ip"] = server->arg("ip");
+          data["gw"] = server->arg("gw");
+          data["sn"] = server->arg("sn");
+       data.printTo(configFile);
+       configFile.close();
+       }
 }
 void broadcast(){
    IPAddress broadcastIp = ~WiFi.subnetMask() | WiFi.gatewayIP();
    
    if(udp.beginPacket(broadcastIp, UPDPORT) == 1){
-    Serial.print(broadcastIp);
+    //Serial.print(broadcastIp);
    }
-   udp.write("IP: ");
    udp.print(WiFi.localIP());
    if(udp.endPacket() == 1){
-    Serial.println(" send endPacket");
+    //Serial.println(" send endPacket");
    }
-
    IPAddress bIp(255, 255, 255, 255);
    if(udp.beginPacket(bIp, UPDPORT) == 1){
-    Serial.print(bIp);
+   // Serial.print(bIp);
    }
-    udp.write("IP: ");
     udp.print(WiFi.localIP());
-    udp.print(WiFi.subnetMask());
    if(udp.endPacket() == 1){
-    Serial.println(" send endPacket");
-   }
-   
-      IPAddress bIp1(10, 0, 0, 185);
-   if(udp.beginPacket(bIp, UPDPORT) == 1){
-    Serial.print(bIp1);
-   }
-    udp.write("IP: ");
-    udp.print(WiFi.localIP());
-    udp.print(WiFi.subnetMask());
-   if(udp.endPacket() == 1){
-    Serial.println(" send endPacket");
-   }
-   
+   //Serial.println(" send endPacket");
+   }   
 }
 //------------- end main functions----------------------------------------  
 void setup() {
@@ -173,9 +234,6 @@ void setup() {
   int start_time = millis(); // remember starttime
   servo.attach(MOTOR_PIN);
   servo.write(0); // initial position
-  
-  timer_remote.setInterval(REPOSITORY_INTERVAL, checkRepository);
-  timer_vcc.setInterval(POWERCHECK_INTERVAL, batteryCheck);
   
   Serial.begin(115200);
   Serial.print("\n Starting at :");
@@ -203,24 +261,40 @@ void setup() {
   } else{
     Serial.print("local ip: ");
     Serial.println(WiFi.localIP());
+    
+    checkConfig(); //check whether config exist otherwise create
+    initVars(); // set vars
+    timer_remote.setInterval(REPOSITORY_INTERVAL, checkRepository);
+    timer_vcc.setInterval(POWERCHECK_INTERVAL, batteryCheck);
+    timer_broadcast.setInterval(BROADCAST_INTERVAL, broadcast);
+    
     Serial.println("Starting http server...");
     server.reset(new ESP8266WebServer(WiFi.localIP(), 80));
         server->on("/whoami", [](){ 
-          server->send(200, "application/json", "{\"device_name\" : \"PetFeed\", \"version\" : " + DEV_VERSION + "}"); 
+          server->send(200, "application/json", "{\"device_name\" : \"PetFeed\", \"version\" : " + DEV_VERSION + ", \"repository_host\" : " + REPOSITORY_HOST + ", \"repository_interval\" : " + REPOSITORY_INTERVAL + ", \"DID\" : " + DID + ", \"DHEX\" : " + DHEX + "}"); 
         });
+        /* delete in production
         server->on("/servo", [](){ 
           goFeed(server->arg("val")); 
         });
-        server->on("/servoslow", [](){
-          goFeedSlow(server->arg("val")); 
+        */
+        server->on("/dofeed", [](){
+          doFeed(server->arg("portion")); 
         });
          server->on("/vcc", [](){
           float vdd = ESP.getVcc();
           server->send(200, "text/plain", String(vdd));
         });
          server->on("/setconfigparams", [](){
-          setConfigParams(server->arg("repository_host"), server->arg("repository_int_check"));
+          setConfigParams(server->arg("repository_host"), server->arg("repository_interval"));
           server->send(200, "text/plain", String("OK"));
+          ESP.reset();
+        });
+        server->on("/setstaticip", [](){
+          //do verify here!!!!!!
+          setStaticIP(server->arg("ip"), server->arg("gw"), server->arg("sn"));
+          server->send(200, "text/plain", String("OK"));
+          ESP.reset();
         });
         server->begin();
         Serial.println("Custom HTTP server started");
@@ -256,5 +330,6 @@ void loop() {
   server->handleClient();
   timer_remote.run();
   timer_vcc.run();
+  timer_broadcast.run();
     }
 }
